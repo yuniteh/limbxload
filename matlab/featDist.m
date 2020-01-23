690fa0f8bb29d638d9be56a6eb42d92c4240b57e
yuniteh <yuniteh2021@u.northwestern.edu>
2020-01-23 15:46:24 -0600

    auto loop through subjects RI function

--- matlab/featDist.m
+++ matlab/featDist.m
@@ -1,12 +1,25 @@
+subAll = loadSubs(subType,1);
 
-d = zeros(4,7);
-
-for pos = 1:4
-    for cl = 1:max(params(:,2))
-        test_feat = feat(params(:,1) == 3 & params(:,2) == cl & params(:,3) == pos,:);
-        stat_feat = feat(params(:,1) == 3 & params(:,2) == cl & params(:,3) == 1,:);
-        d(pos,cl) = .5*mean(sqrt(mahal(test_feat,stat_feat)));
+for subInd = 1:size(subAll.subs,1)
+    sub = subAll.subs{subInd};
+    path = ['Z:\Lab Member Folders\Yuni Teh\projects\limbxload\matlab\completed\' subType '\' sub '\DATA\MAT'];
+    if exist(fullfile(path,'train_data.mat'),'file')
+        load(fullfile(path,'train_data.mat'));
+        disp(subAll.subs{subInd})
+        d = zeros(4,7);
+        v = d;
+        for pos = 1:4
+            for cl = 1:max(params(:,2))
+                test_feat = feat(params(:,1) == 3 & params(:,2) == cl & params(:,3) == pos,:);
+                stat_feat = feat(params(:,1) == 3 & params(:,2) == cl & params(:,3) == 1,:);
+                if ~isempty(stat_feat)
+                    d(pos,cl) = mean(sqrt(mahal(test_feat,stat_feat)));
+                    v(pos,cl) = var(sqrt(mahal(test_feat,stat_feat)));
+                end
+            end
+        end
+        
+        d
+        v
     end
 end
-
-d
--- matlab/statsTest.m
+++ matlab/statsTest.m
@@ -12,10 +12,10 @@ all = array2table(data_all(cInd,:),'VariableNames',{'Sub','Load','Train','Pos','
 stat_nl = array2table(data_all(data_all(:,3) == 1 & cInd & data_all(:,2) == 1,:),'VariableNames',{'Sub','Load','Train','Pos','DOF','Complete','Time','Path','In','PMove','Distance','MaxIn','Sliding','Eff','Accuracy'});
 
 %% ASSIGN CATEGORICAL FACTORS
-all.Pos = nominal(all.Pos);
-all.Sub = nominal(all.Sub);
-all.Train = nominal(all.Train);
-all.Load = nominal(all.Load);
+all.Pos = categorical(all.Pos,[1,2,3,4]);
+all.Sub = categorical(all.Sub);
+all.Train = categorical(all.Train,[1,2]);
+all.Load = categorical(all.Load,[1,2,3]);
 
 static.Pos = categorical(static.Pos,[1,2,3,4]);
 static.Sub = categorical(static.Sub);
@@ -37,3 +37,8 @@ stat_nl.Sub = nominal(stat_nl.Pos);
 %%
 mod = fitlme(static,'Accuracy~Pos+Load+Pos*Load+(1|Sub)')%,'dummyvarcoding','effects')
 anova(mod)
+
+%%
+
+mod = fitlme(all,'Eff~Pos + Load + Train + Pos*Load + Load*Train + Pos*Train + (1|Sub)')
+rmod = fitlme(all,'Eff~Pos + Load + Pos*Load + (1|Sub)')
