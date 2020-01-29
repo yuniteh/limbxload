4466a5cfa31f0b4dc8d86d86ca3b13d322c5b4bd
yuniteh <yuniteh2021@u.northwestern.edu>
2020-01-29 14:48:51 -0600

    added confusion matrix like things to featdist

--- matlab/featDist.m
+++ matlab/featDist.m
@@ -25,19 +25,21 @@ for subInd = 1:size(subAll.subs,1)
                         stat_cen = mean(stat_feat);         % static data centroid
                         stat_ax = 2.*std(stat_feat);        % static data semi-principal axis
                         m_dist{load,pos,cl} = sqrt(mahal(test_feat,stat_feat));
-                        RI(load,pos,cl) = 0.5*sqrt(mahal(mean(test_feat),stat_feat));
+                        RI(load,pos,cl) = modmahal(test_feat,stat_feat);
                         MSA(load,pos,cl) = geomean(test_ax);
                         
                         % SI calculation
                         for c = 1:max(params(:,2))
                             %if c ~= cl
-                                SI_feat = feat(params(:,1) == 3 & params(:,2) == c & params(:,3) == pos,:);
-                                SI_temp = 0.5*sqrt(mahal(mean(test_feat),SI_feat));
-                                if SI_temp < SI(load,pos,cl) || isnan(SI(load,pos,cl))
+                            SI_feat = feat(params(:,1) == 3 & params(:,2) == c & params(:,3) == 1,:);
+                            SI_temp = modmahal(test_feat,SI_feat);
+                            if SI_temp < SI(load,pos,cl) || isnan(SI(load,pos,cl))
+                                if c ~= cl
                                     SI(load,pos,cl) = SI_temp;
-                                    SI_class(load,pos,cl) = c;
                                 end
-                           %end
+                                SI_class(load,pos,cl) = c;
+                            end
+                            %end
                         end
                     end
                 end
@@ -69,5 +71,26 @@ for subInd = 1:size(subAll.subs,1)
         end
     end
     clearvars -except subInd nPos nLoad subAll subType data_train
+end
+
+%%
+RI_mat = cell(max(data_train(:,2)),1);
+MSA_mat = RI_mat;
+SI_c = cell(max(data_train(:,2)),max(data_train(:,3)));
+
+for load = 1:max(data_train(:,2))
+    RI_mat{load} = nan(max(data_train(:,3)),max(data_train(:,4)));
+    MSA_mat{load} = nan(max(data_train(:,3)),max(data_train(:,4)));
+    for pos = 1:max(data_train(:,3))
+        SI_c{load,pos} = zeros(max(data_train(:,4)));
+        for cl = 1:max(data_train(:,4))
+            ind = data_train(:,2) == load & data_train(:,3) == pos & data_train(:,4) == cl;
+            RI_mat{load}(pos,cl) = nanmean(data_train(ind,5));
+            MSA_mat{load}(pos,cl) = nanmean(data_train(ind,7));
+            for c2 = 1:max(data_train(:,4))
+                SI_c{load,pos}(cl,c2) = SI_c{load,pos}(cl,c2) + sum(data_train(ind,end) == c2);
+            end
+        end
+    end
 end
 
